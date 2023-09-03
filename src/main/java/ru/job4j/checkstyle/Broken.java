7e99d8afb0ffdf69a3edd7467b830cb7c9bca1bb
XLoganxmenX <loganxmen97@gmail.com>
2023-09-03 19:49:33 +0900

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private String name;
+    private String surname;
+    public static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+
+    Broken() {
+
+    }
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e) {
+
+    }
+}
