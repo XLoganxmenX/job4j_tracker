dcc002367104dcfdf9fa37809ecbd93bacc7269f
XLoganxmenX <loganxmen97@gmail.com>
2023-09-07 22:02:03 +0900

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,10 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setFullName("Nikiforov Archibald");
+        student1.setGroup("KPD-9-23");
+        student1.setDateOfEntrance("06.07.2023");
+    }
+}
