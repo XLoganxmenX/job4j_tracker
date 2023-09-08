154a27bb0a6e3f2000911bca9e1f595886b61219
XLoganxmenX <loganxmen97@gmail.com>
2023-09-08 19:41:59 +0900

    2. Модель данных. [#174683 #443290]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -6,5 +6,9 @@ public class College {
         student1.setFullName("Nikiforov Archibald");
         student1.setGroup("KPD-9-23");
         student1.setDateOfEntrance("06.07.2023");
+
+        System.out.println(student1.getFullName());
+        System.out.println(student1.getGroup());
+        System.out.println(student1.getDateOfEntrance());
     }
 }
