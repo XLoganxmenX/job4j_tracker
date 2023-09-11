972131c56589f8f3e14d26bf0c0f60b28a2feb1f
XLoganxmenX <loganxmen97@gmail.com>
2023-09-11 22:01:51 +0900

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/Collage.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Collage {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student studFresh = freshman;
+        Object objFresh = freshman;
+    }
+}
