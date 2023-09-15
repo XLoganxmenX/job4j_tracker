df3c191ef8db77c89f1d4a14d4e30a1f1e1c1240
XLoganxmenX <loganxmen97@gmail.com>
2023-09-15 12:02:59 +0900

    0.5. Иерархия исключений и множественный catch. [#219368 #445001]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -7,6 +7,7 @@ public class UserStore {
         for (User user : users) {
             if (user.getUsername().equals(login)) {
                 findUser = user;
+                break;
             }
         }
         if (findUser == null) {
