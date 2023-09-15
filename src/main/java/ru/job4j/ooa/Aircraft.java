25c2ee464a2c252159e4addabe32ead4b11d5f52
XLoganxmenX <loganxmen97@gmail.com>
2023-09-15 13:47:24 +0900

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Aircraft.java
@@ -0,0 +1,12 @@
+package ru.job4j.ooa;
+
+public class Aircraft {
+
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+
+    public void printCountEngine() {
+        System.out.println("Метод выводит в консоль количество двигателей самолета.");
+    }
+}
