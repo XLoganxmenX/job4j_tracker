4a7f607b8428864301ffa533f2eb013ae5f19844
XLoganxmenX <loganxmen97@gmail.com>
2023-09-15 11:55:27 +0900

    0.3. Тестирование исключений с junit [#219366 #444995]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -15,4 +15,12 @@ class FactTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less than 0");
     }
+
+    @Test
+    public void when3Than6() {
+        int inputNum = 3;
+        int expect = 6;
+        int rsl = Fact.calc(inputNum);
+        assertThat(rsl).isEqualTo(expect);
+    }
 }
