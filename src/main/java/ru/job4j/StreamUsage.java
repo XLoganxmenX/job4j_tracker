d911e2428516c17a014a72fa1ebb14e8543923b6
XLoganxmenX <loganxmen97@gmail.com>
2023-09-28 20:27:57 +0900

    3. Отладка лямбда в IDEA [#259165 #448455]

--- src/main/java/ru/job4j/StreamUsage.java
+++ src/main/java/ru/job4j/StreamUsage.java
@@ -20,7 +20,7 @@ public class StreamUsage {
                 new Task("Bug #3", 40)
         );
         tasks.stream().filter(task -> task.name.contains("Bug"))
-                .filter(task -> task.spent == 30)
+                .filter(task -> task.spent > 30)
                 .map(task -> task.name + " " + task.spent)
                 .forEach(System.out::println);
     }
