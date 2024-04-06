3a895694d271ad692d6123ebdf5b154745d1d88a
XLoganxmenX <loganxmen97@gmail.com>
2024-04-06 13:08:27 +0900

    add CreateManyItems, DeleteAllItems and Profile doc [#504894]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -33,11 +33,11 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        try (Store tracker = new SqlTracker()) {
+        try (Store tracker = new MemTracker()) {
             List<UserAction> actions = List.of(
                     new CreateAction(output), new ShowAllAction(output), new ReplaceAction(output),
                     new DeleteAction(output), new FindByIdAction(output), new FindByNameAction(output),
-                    new ExitAction(output)
+                    new ExitAction(output), new CreateManyItems(output), new DeleteAllItems(output)
             );
             new StartUI(output).init(input, tracker, actions);
         } catch (Exception e) {
