959b70125bea3e075e054d21c764bc827e84a47f
XLoganxmenX <loganxmen97@gmail.com>
2024-08-06 21:53:18 +0900

    add Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,57 +2,32 @@ package ru.job4j.tracker;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-@Setter
-@Getter
+@Entity
+@Table(name = "items")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructor
+@EqualsAndHashCode
 public class Item implements Comparable<Item> {
-    @NonNull
-    private int id;
-    @NonNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
+
+    @EqualsAndHashCode.Exclude
     private LocalDateTime created = LocalDateTime.now();
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item(String name) {
         this.name = name;
     }
 
     @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
-                + '}';
-    }
-
-    @Override
-    public int compareTo(Item another) {
+    public int compareTo(@NonNull Item another) {
         return this.name.compareTo(another.name);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id
-                && Objects.equals(name, item.name)
-                && Objects.equals(created.format(FORMATTER), item.created.format(FORMATTER));
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, created);
     }
 }
